907dfa8d372265668d9633b7ad639de15e80c9cb
Yuriy Lemaykin <y.lemaykin@gmail.com>
2023-01-23 23:12:30 +0300

    Disposable object created

--- app/src/main/java/com/gmiller/todolist/AddNoteViewModel.java
+++ app/src/main/java/com/gmiller/todolist/AddNoteViewModel.java
@@ -1,14 +1,18 @@
 package com.gmiller.todolist;
 
 import android.app.Application;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
-import io.reactivex.rxjava3.core.Scheduler;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.functions.Action;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
@@ -16,6 +20,7 @@ public class AddNoteViewModel extends AndroidViewModel {
 
     private NotesDao notesDao;
     private MutableLiveData<Boolean> shouldCloseScreen = new MutableLiveData<>();
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
 
     public AddNoteViewModel(@NonNull Application application) {
         super(application);
@@ -27,15 +32,36 @@ public class AddNoteViewModel extends AndroidViewModel {
     }
 
     public void saveNote(Note note) {
-        notesDao.add(note)
+        Disposable disposable = notesDao.add(note)
+                .delay(5, TimeUnit.SECONDS)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Action() {
                     @Override
                     public void run() throws Throwable {
+                        Log.d("AddNoteViewModel", "Subscribe");
                         shouldCloseScreen.setValue(true);
                     }
                 });
+        compositeDisposable.add(disposable);
+    }
 
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        compositeDisposable.dispose();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
